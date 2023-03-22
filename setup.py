0ebb3c03b5c2d8e8ce55d29d401f1ca183d750f1
Alex Van Brunt <avanbrunt@carbonblack.com>
2023-03-21 19:28:00 -0600

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,4 @@
-"""
-cbc-syslog
-"""
+"""cbc-syslog"""
 
 from setuptools import setup
 from setuptools import find_packages
@@ -8,23 +6,31 @@ import io
 
 
 install_requires = [
-    'Jinja2==2.11.3',
-    'requests==2.24.0',
-    'psutil==5.7.3',
+    "carbon-black-cloud-sdk",
+    "Jinja2",
+    "psutil"
+]
+
+extras_require = {
+    "test": [
+        'pytest==7.2.1',
+        'coverage==6.5.0',
+        'coveralls==3.3.1',
+        'flake8==5.0.4',
+        'flake8-colors==0.1.9',
+        'flake8-docstrings==1.7.0',
+        'pre-commit>=2.15.0',
     ]
+}
 
 packages = [
     'cbc_syslog',
-    'cbc_syslog.root'
-    ]
+]
 
 with io.open('README.md', 'rt', encoding='utf8') as f:
     long_description = f.read()
 
-scripts = ['src/cbc_syslog/root/usr/share/cb/integrations/cbc-syslog/cacert.pem',
-           'src/cbc_syslog/root/etc/cron.d/cbc-syslog',
-           'src/cbc_syslog/root/etc/cb/integrations/cbc-syslog/cbc-syslog.conf.example'
-           ]
+scripts = []
 
 setup(
     name='cbc_syslog',
@@ -40,17 +46,15 @@ setup(
     author_email='cb-developer-network@vmware.com',
     description='Syslog Connector for the Carbon Black Cloud',
     install_requires=install_requires,
+    extras_require=extras_require,
     classifiers=[
-
         # Indicate who your project is intended for
         'Intended Audience :: Developers',
 
         # Pick your license as you wish (should match "license" above)
         'License :: OSI Approved :: MIT License',
 
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        ],
+    ],
     keywords='carbonblack',
     scripts=scripts)
