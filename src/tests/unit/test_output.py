9ccf023d67e9cc233532aa7aeb912f7e47cc6fd4
Alex Van Brunt <avanbrunt@carbonblack.com>
2023-04-17 17:50:42 -0600

    Add tcp+tls testing

--- src/tests/unit/test_output.py
+++ src/tests/unit/test_output.py
@@ -11,10 +11,13 @@
 
 """Tests for the Output object."""
 
+import pathlib
 import pytest
 import time
 
 from cbc_syslog.util import Output
+
+CERTS_PATH = pathlib.Path(__file__).joinpath("../../fixtures/certs").resolve()
 
 
 @pytest.mark.filterwarnings("ignore:Unverified HTTPS request.*")
@@ -30,6 +33,18 @@ def test_send_http():
     assert "Hello World".encode("utf-8") == pytest.http_recv_data
 
 
+@pytest.mark.filterwarnings("ignore:Unverified HTTPS request.*")
+def test_send_http_invalid():
+    """Test HTTP transmission with invalid port"""
+    output = Output({
+        "type": "http",
+        "host": "https://0.0.0.0:1234/http_out",
+        "tls_verify": False
+    })
+    success = output.send("Hello World")
+    assert success is False
+
+
 def test_send_tcp():
     """Test TCP transmission"""
     output = Output({
@@ -40,6 +55,46 @@ def test_send_tcp():
     success = output.send("Hello World")
     assert success is True
     assert "Hello World".encode("utf-8") == pytest.tcp_recv_data
+
+
+def test_send_tcp_invalid():
+    """Test TCP transmission with invalid port"""
+    output = Output({
+        "type": "tcp",
+        "host": "0.0.0.0",
+        "port": "1234"
+    })
+    success = output.send("Hello World")
+    assert success is False
+
+
+def test_send_tcp_tls():
+    """Test TCP + TLS transmission"""
+    output = Output({
+        "type": "tcp+tls",
+        "host": "localhost",
+        "port": "8888",
+        "ca_cert": str(CERTS_PATH.joinpath("rootCACert.pem"))
+    })
+    success = output.send("Hello World")
+    assert success is True
+
+    # Add small delay to enable tls handshake to complete
+    time.sleep(0.1)
+    assert "Hello World".encode("utf-8") == pytest.tcp_tls_recv_data
+
+
+def test_send_tcp_tls_mismatch_host(caplog):
+    """Test TCP + TLS transmission with mismatch host to CA cert"""
+    output = Output({
+        "type": "tcp+tls",
+        "host": "0.0.0.0",
+        "port": "8888",
+        "ca_cert": str(CERTS_PATH.joinpath("rootCACert.pem"))
+    })
+    success = output.send("Hello World")
+    assert success is False
+    assert "CERTIFICATE_VERIFY_FAILED" in caplog.records[0].msg
 
 
 def test_send_udp():
@@ -55,3 +110,14 @@ def test_send_udp():
     # Add small delay to enable udp transaction to complete
     time.sleep(0.1)
     assert "Hello World".encode("utf-8") == pytest.udp_recv_data
+
+
+def test_send_udp_invalid():
+    """Test UDP transmission with invalid ip"""
+    output = Output({
+        "type": "udp",
+        "host": "bad",
+        "port": "1234"
+    })
+    success = output.send("Hello World")
+    assert success is False
